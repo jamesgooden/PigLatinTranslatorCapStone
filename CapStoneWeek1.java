b0c8e7066c0cb75c82cb9ac117ffa1ed7fcb0a1b
james <jamesgooden0307@gmail.com>
2018-10-14 22:14:57 -0400

    Pig Latin

--- CapStoneWeek1.java
+++ CapStoneWeek1.java
@@ -4,50 +4,61 @@ public class CapStoneWeek1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		String userWord;
 		String userContinue;
+		int atSymbol;
 
 		System.out.println("Welcome to the Pig Latin Translator");
 
-		do {
-			System.out.println("Enter a line to be translated:   ");
-			String userWord = scan.next();
-			System.out.println(translateToPigLatin(userWord));
-			System.out.println("Would you like continue Y/N?");
+		do { 
+			System.out.println("Type a word you would like to translate:  ");
+			userWord = scan.next();
+			atSymbol = userWord.indexOf('@'); // keeps entries with the @ symbol the same
+			if (atSymbol > 0) {
+				System.out.println(userWord);
+			} else if (userWord.length() <= 1) {
+				System.out.println("Please enter a word with atleast 2 characters");  // validates user entry for words with enough characters
+			}
+
+			else
+				System.out.println(translateToPigLatin(userWord.toLowerCase())); // turns user input into Pig Latin / converts to lower case
+			System.out.println("Translate another word? (y/n) ");
 			userContinue = scan.next();
+
 		} while (userContinue.equalsIgnoreCase("Y"));
 		System.out.print("Thank you for playing! ");
 		System.out.println("Goodbye.");
-
+		scan.close();
 	}
 
-	public static String translateToPigLatin(String string) {
+	public static String translateToPigLatin(String string) {   //  This is  where the words are translated based on what the index of the vowel is
 		String end = "";
 		String spacing = "\\s+";
 		String[] words = string.split(spacing);
 		for (int i = 0; i < words.length; i++) {
-			if (vowelChecker(words[i].charAt(0))) {
+			if (vowelChecker(words[i].charAt(0))) {             
 				end += words[i] + "way";
-            } 
-			   else if  (vowelChecker(words[i].charAt(1)))
-		       {
-					end += words[i].substring(1) + words[i].substring(0, 1) + "ay";
-				}
-			else 	if ( vowelChecker(words[i].charAt(2))) {
-				end += words[i].substring(2) + words[i].substring(0,2) + "ay" ;
-		   	}
-			else if ( vowelChecker(words[i].charAt(3))) {
-				end += words[i].substring(3) + words[i].substring(0,3) + "ay" ;
+			} else if (vowelChecker(words[i].charAt(1))) {
+				end += words[i].substring(1) + words[i].substring(0, 1) + "ay";
+			} else if (vowelChecker(words[i].charAt(2))) {
+				end += words[i].substring(2) + words[i].substring(0, 2) + "ay";
+			} else if (vowelChecker(words[i].charAt(3))) {
+				end += words[i].substring(3) + words[i].substring(0, 3) + "ay";
+			} else if (vowelChecker(words[i].charAt(4))) {
+				end += words[i].substring(4) + words[i].substring(0, 4) + "ay";
+			} else if (vowelChecker(words[i].charAt(5))) {
+				end += words[i].substring(5) + words[i].substring(0, 5) + "ay";
+			} else {
+
+				end += words[i] + "way";
 			}
-			else if  (vowelChecker(words[i].charAt(4))) {
-				end+= words [i].substring(4)+ words[i].substring(0,4) + "ay";
-			}
-			
+
 		}
 		return end;
 
 	}
 
-	public static boolean vowelChecker(char c) {
+	public static boolean vowelChecker(char c) {   //This tells the translator if the character has a vowel
 		if (c == 'a' || c == 'A')
 			return true;
 		if (c == 'e' || c == 'E')
